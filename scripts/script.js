bd0536a1e536f14fc8562ef7a524063f9c87aada
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2022-09-07 17:32:18 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+const drinkInfo = document.querySelector('#drink-info');
+let description = document.getElementById('description');
+let hot = document.getElementById('hot');
+let cold = document.getElementById('cold');
+let drink = document.getElementById('drinks').value;
+
+let latte = {
+    description: "Latte's are a coffee beverage made with espresso and steamed milk.",
+    hot: "A latte is made with any optional syrup flavors, two shots and the rest steamed milk.",
+    cold: "An ice latte is made the same way with steamed milk substituted for cold milk."
+};
+
+let cappucino = {
+    description: "Similar to a latte, cappucino's contain a more frosty "
+}
+
+let breve = {
+    description: "A breve is the same as a latte only with half and half substitued for the milk."
+}
+
+if (drink === "latte") {
+    description.textContent = latte.description;
+    hot.textContent = latte.hot;
+    cold.textContent = latte.cold;
+}
