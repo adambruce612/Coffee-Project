5d8f3553636aa3a016564c6247333ecbdfc47032
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2022-10-03 17:28:46 -0500

    Add files via upload
    Updated to JavaScript (work on event listener for select list needed). Update to HTML select list for drinks listed.

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,52 @@ const drinkInfo = document.querySelector('#drink-info');
 let description = document.getElementById('description');
 let hot = document.getElementById('hot');
 let cold = document.getElementById('cold');
-let drink = document.getElementById('drinks').value;
+let drinks = document.getElementById('drinks');
 const descriptionLabel = "Description:";
 
 let latte = {
-    description: "Latte's are a coffee beverage made with espresso and steamed milk.",
+    description: "A type of coffee made with espresso and hot steamed milk, milkier than a cappuccino. (Google Oxford Language Definition)",
     hot: "A latte is made with any optional syrup flavors, two shots and the rest steamed milk.",
-    cold: "An ice latte is made the same way with steamed milk substituted for cold milk."
+    cold: "An iced latte is made the same way with as hot but with steamed milk substituted for cold milk."
 };
 
 let cappucino = {
-    description: "Similar to a latte, cappucino's contain a more frosty "
+    description: "Coffee made with milk that has been frothed up with pressurized steam. (Google Oxford Language Definition)",
+    hot: "A cappucino is made with any optional syrup flavors, two shots and the rest steamed milk.",
+    cold: "An iced cappucino is made the same way with as hot but with steamed milk substituted for cold milk."
 }
 
 let breve = {
-    description: "A breve is the same as a latte only with half and half substitued for the milk."
+    description: "A breve is the same as a latte only with half and half substitued for the milk.",
+    hot: "A breve is made with any optional syrup flavors, two shots and the rest steamed milk.",
+    cold: "An iced breve is made the same way with as hot but with steamed milk substituted for cold milk."
 }
 
-if (drink === "latte") {
-    description.textContent = descriptionLabel + " " + latte.description;
-    hot.textContent = latte.hot;
-    cold.textContent = latte.cold;
+let redEye = {
+    description: "A red eye is brewed drip coffee combined with espresso.",
+    hot: "A hot red eye will use hot drip coffee and then the desired amount of shots plus any cream, sugar, or syrup flavors requested by the customer.",
+    iced: "An iced red eye will be the made the same way as hot with cold brew substituted for the drip coffe and ice added."
+}
+
+drinks.addEventListener('change', console.log(drinks));
+
+function selectedDrink(value) {
+    let drink = drinks.value;
+    console.log(drink);
+    if (drink === "latte") {
+        displayContent(latte);        
+    } else if (drink === "cappucino") {
+        displayContent(cappucino)
+    } else if (drink === "breve") {
+        displayContent(breve);
+    } else if (drink === "redEye") {
+        displayContent(redEye)
+    }
+};
+
+
+function displayContent(drink) {
+    description.textContent = descriptionLabel + " " + drink.description;
+    hot.textContent = drink.hot;
+    cold.textContent = drink.cold;
 }
